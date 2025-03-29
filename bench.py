0d51cb48776a17cf06094b1054e161e1e05cd274
daisy petals <da1sypetals.iota@gmail.com>
2025-03-29 17:03:59 +0800

    tested

--- bench.py
+++ bench.py
@@ -1,39 +1,56 @@
-import math
-
 import torch
-from torch.nn import functional as F
 from torch.utils.cpp_extension import load
+import einops as ein
 
 # Load the CUDA kernel as a python module
-minimal_attn = load(name='minimal_attn', sources=['main.cpp', 'flash.cu'], extra_cuda_cflags=['-O2'])
+minimal_attn = load(
+    name="minimal_attn", sources=["main.cpp", "flash.cu"], extra_cuda_cflags=["-O2"]
+)
 
 # Use small model params, otherwise slower than manual attention. See caveats in README.
-batch_size = 16
+batch_size = 1
 n_head = 12
 seq_len = 64
 head_embd = 64
+mask_embed = 8
 
 q = torch.randn(batch_size, n_head, seq_len, head_embd).cuda()
 k = torch.randn(batch_size, n_head, seq_len, head_embd).cuda()
 v = torch.randn(batch_size, n_head, seq_len, head_embd).cuda()
+mf = (torch.rand(batch_size, seq_len, mask_embed) < 0.5).to(dtype=torch.int32)
+# mf = torch.ones(batch_size, seq_len, seq_len, dtype=torch.int32)
+mask = (ein.einsum(mf, mf, "b l1 d, b l2 d -> b l1 l2") > 0).to(torch.bool)
+mf = mf.cuda()
+mask = ein.repeat(mask, "b l1 l2 -> b h l1 l2", h=n_head).cuda()
+print(mask.sum().item())
+print(batch_size * n_head * seq_len * seq_len)
 
-print('=== profiling manual attention ===')
 
 # Our minimal flash attention aims to be faster than this by avoiding HBM read/writes of N^2 matrices.
-def manual_attn(q, k, v):
-    att = (q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1))))
-    att = F.softmax(att, dim=-1)
-    y = att @ v
-    return y
+def manual_attn(q, k, v, mask):
+    res = torch.nn.functional.scaled_dot_product_attention(q, k, v, mask)
+    return res
+
+
+print("=== profiling manual attention ===")
+
 
 with torch.autograd.profiler.profile(use_cuda=True) as prof:
-    manual_result = manual_attn(q, k, v)
-print(prof.key_averages().table(sort_by='cuda_time_total', row_limit=10))
+    manual_result = manual_attn(q, k, v, mask)
+print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
 
-print('=== profiling minimal flash attention === ')
+print("=== profiling minimal flash attention === ")
 
 with torch.autograd.profiler.profile(use_cuda=True) as prof:
-    minimal_result = minimal_attn.forward(q, k, v)
-print(prof.key_averages().table(sort_by='cuda_time_total', row_limit=10))
+    minimal_result = minimal_attn.forward(q, k, v, mf)
+print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
 
-print('attn values sanity check:', torch.allclose(minimal_result, manual_result, rtol=0, atol=1e-02))
+
+diff = minimal_result - manual_result
+print(f"Max manual: {manual_result.abs().max()}")
+print(f"Max diff: {diff.abs().mean() / manual_result.abs().mean()}")
+
+print(
+    "attn values sanity check:",
+    torch.allclose(minimal_result, manual_result, rtol=0, atol=1e-2),
+)
--- flash.cu
+++ flash.cu
@@ -5,23 +5,32 @@
 __global__
 void forward_kernel(const float* Q, const float* K, const float* V, const int N, const int d,
                     const int Tc, const int Tr, const int Bc, const int Br, const float softmax_scale,
-                    float* l, float *m, float* O) {
+                    float* l, float *m, float* O, int* M, const int dm) {
     int tx = threadIdx.x;
     int bx = blockIdx.x; int by = blockIdx.y;  // batch and head index
 
     // Offset into Q,K,V,O,l,m - different for each batch and head
     int qkv_offset = (bx * gridDim.y * N * d) + (by * N * d);  // gridDim.y = nh
+    int mask_offset = bx * N * dm;
     int lm_offset = (bx * gridDim.y * N) + (by * N);  // offset for l and m
 
     // Define SRAM for Q,K,V,S
     extern __shared__ float sram[];
     int tile_size = Bc * d;  // size of Qi, Kj, Vj
+    int m_tile_size = Bc * dm;
     float* Qi = sram;
     float* Kj = &sram[tile_size];
     float* Vj = &sram[tile_size * 2];
-    float* S = &sram[tile_size * 3];
+    float* Mi = &sram[tile_size * 3];
+    float* Mj = &sram[tile_size * 3 + m_tile_size];
+    float* S = &sram[tile_size * 3 + m_tile_size * 2];
+    // float* mask = &sram[tile_size * 3 + m_tile_size * 2 + Br * Bc];
 
     for (int j = 0; j < Tc; j++) {
+        // load Mj to SRAM
+        for (int x = 0; x < dm; x++) {
+            Mj[(tx * dm) + x] = M[mask_offset + (m_tile_size * j) + (tx * dm) + x];
+        }
 
         // Load Kj, Vj to SRAM
         for (int x = 0; x < d; x++) {
@@ -31,6 +40,11 @@ void forward_kernel(const float* Q, const float* K, const float* V, const int N,
         __syncthreads();  // such that the inner loop can use the correct Kj, Vj
 
         for (int i = 0; i < Tr; i++)  {
+
+            // Load Mi to SRAM
+            for (int x = 0; x < dm; x++) {
+                Mi[(tx * dm) + x] = M[mask_offset + (m_tile_size * i) + (tx * dm) + x];
+            }
 
             // Load Qi to SRAM, l and m to registers
             for (int x = 0; x < d; x++) {
@@ -51,6 +65,17 @@ void forward_kernel(const float* Q, const float* K, const float* V, const int N,
 
                 if (sum > row_m)
                     row_m = sum;
+            }
+
+            // mask = Mi @ Mj^T, apply mask bias
+            for (int y = 0; y < Bc; y++) {
+                int masksum = 0;
+                for (int x = 0; x < dm; x++) {
+                    masksum += Mi[(tx * dm) + x] * Mj[(y * dm) + x];
+                }
+                printf("(%d)", masksum > 0);
+                // mask[(Bc * tx) + y] = masksum > 0 ? 1.0f : 0.0f;
+                S[(Bc * tx) + y] += masksum > 0 ? 0.0 : -INFINITY;
             }
 
             // P = exp(S - row_m), row_l = rowsum(P)
@@ -81,12 +106,16 @@ void forward_kernel(const float* Q, const float* K, const float* V, const int N,
     }
 }
 
-torch::Tensor forward(torch::Tensor Q, torch::Tensor K, torch::Tensor V) {
+torch::Tensor forward(torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor mask_flag) { // mask_flag: type=int32, shape=(bsz, seqlen, dm)
     // TODO: determine Bc, Br dynamically
-    const int Bc = 32; const int Br = 32;
+    const int Bc = 32; 
+    const int Br = 32;
+    // assert(Bc == Br);
 
     const int B = Q.size(0); const int nh = Q.size(1);
     const int N = Q.size(2); const int d = Q.size(3);
+
+    const int dim_mask = mask_flag.size(2);
 
     const int Tc = ceil((float) N / Bc); const int Tr = ceil((float) N / Br);
     const float softmax_scale = 1.0 / sqrt(d);
@@ -99,7 +128,16 @@ torch::Tensor forward(torch::Tensor Q, torch::Tensor K, torch::Tensor V) {
     l = l.to(device); m = m.to(device);
 
     // Calculate SRAM size needed per block
-    const int sram_size = (3 * Bc * d * sizeof(float)) + (Bc * Br * sizeof(float));
+    int Qi_size = Br * d; 
+    int Kj_size = Bc * d; 
+    int Vj_size = Bc * d; 
+    int S_size = Br * Bc; 
+    int mask_size = Br * Bc; 
+    int sub_mask_flag_size_row = Br * dim_mask;
+    int sub_mask_flag_size_col = Bc * dim_mask;
+    const int sram_size = ((Qi_size + Kj_size + Vj_size + S_size) * sizeof(float)) + ((sub_mask_flag_size_row + sub_mask_flag_size_col) * sizeof(int32_t));
+    // const int sram_size = ((Qi_size + Kj_size + Vj_size + S_size + mask_size) * sizeof(float)) + ((sub_mask_flag_size_row + sub_mask_flag_size_col) * sizeof(int32_t));
+
     int max_sram_size;
     cudaDeviceGetAttribute(&max_sram_size, cudaDevAttrMaxSharedMemoryPerBlock, 0);
     printf("Max shared memory: %d, requested shared memory: %d \\n", max_sram_size, sram_size);
@@ -110,7 +148,8 @@ torch::Tensor forward(torch::Tensor Q, torch::Tensor K, torch::Tensor V) {
     forward_kernel<<<grid_dim, block_dim, sram_size>>>(
         Q.data_ptr<float>(), K.data_ptr<float>(), V.data_ptr<float>(),
         N, d, Tc, Tr, Bc, Br, softmax_scale,
-        l.data_ptr<float>(), m.data_ptr<float>(), O.data_ptr<float>()
+        l.data_ptr<float>(), m.data_ptr<float>(), O.data_ptr<float>(),
+        mask_flag.data_ptr<int32_t>(), dim_mask
     );
     return O;
 }
